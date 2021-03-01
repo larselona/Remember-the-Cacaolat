80366bc3d51247ff582cc8b320000800f6b897ba
Lars Isdahl <lars.isdahl@gmail.com>
2021-03-01 16:40:39 +0100

    Update the Attributes in the Entity ok

--- Remember the Cacaolat/Controllers/TodoListViewController.swift
+++ Remember the Cacaolat/Controllers/TodoListViewController.swift
@@ -47,10 +47,10 @@ class TodoListViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-        
+        // 'itemArray[indexPath.row].setValue("Completed", forKey: "title")
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
         
-        self.saveItems()
+        saveItems()
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
