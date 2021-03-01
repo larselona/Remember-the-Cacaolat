c4032fc7b30b25897e51a916882b47f1fbfa8a76
Lars Isdahl <lars.isdahl@gmail.com>
2021-03-01 15:12:04 +0100

    Read and load the Items in the CoreData database on startup

--- Remember the Cacaolat/Controllers/TodoListViewController.swift
+++ Remember the Cacaolat/Controllers/TodoListViewController.swift
@@ -20,7 +20,7 @@ class TodoListViewController: UITableViewController {
         print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
         
         
-//       loadItems()
+       loadItems()
         
     }
     
@@ -98,17 +98,19 @@ class TodoListViewController: UITableViewController {
         self.tableView.reloadData()
     }
     
-//    func loadItems() {
-//        if let data = try? Data(contentsOf: dataFilePath!) {
-//            let decoder = PropertyListDecoder()
-//            do {
-//            itemArray = try decoder.decode([Item].self, from: data)
-//            } catch {
-//                print("Error decoding item array, \(error)")
-//            }
-//        }
-//    }
-    
-    
-    
+    func loadItems() {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        do {
+        itemArray = try context.fetch(request)
+        } catch {
+            print("Error fetching data from context \(error)")
+        }
+    }
+
+
+
+
 }
+
+
+
